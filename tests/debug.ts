fd8741ef74da0e4ae26757076e8c32592b1d0d7b
André Biseth <andre@biseth.net>
2021-11-29 19:02:12 +0100

    Added debug code

--- tests/debug.ts
+++ tests/debug.ts
@@ -6,27 +6,36 @@ import { Zappi } from '../src/models/Zappi';
 
 dotenv.config();
 
-const runner = new Promise<Zappi>(async (resolve, reject) => {
+const runner = new Promise<any>(async (resolve, reject) => {
     const myenergi = new MyEnergi(process.env.USERNAME as string, process.env.PASSWORD as string);
-    
+
     const statusAll = await myenergi.getStatusAll()
     console.log(statusAll);
 
     const zappiAll = await myenergi.getStatusZappiAll();
     console.log(zappiAll);
-    
-    const sno: string = zappiAll[0].sno;
+
+    let sno: string = zappiAll[0].sno;
     const chargeMode = await myenergi.setZappiChargeMode(sno, ZappiChargeMode.Fast);
     console.log(chargeMode);
-    
+
     const boostMode = await myenergi.setZappiBoostMode(sno, ZappiBoostMode.Stop);
     console.log(boostMode);
-    
+
     const greenLevel = await myenergi.setZappiGreenLevel(sno, 0);
     console.log(greenLevel);
-    
+
     const statusZappi = await myenergi.getStatusZappi(sno);
-    resolve(statusZappi);
+    console.log(statusZappi);
+
+    const harviAll = await myenergi.getStatusHarviAll();
+    console.log(harviAll);
+
+    sno = harviAll[0].sno;
+    const statusHarvi = await myenergi.getStatusHarvi(sno);
+    console.log(statusHarvi);
+
+    resolve('OK');
 });
 
 runner.then(x => {
