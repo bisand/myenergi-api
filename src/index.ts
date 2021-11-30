751be28ddce8ab209bc5a7ebfda37754094830ce
André Biseth <andre@biseth.net>
2021-11-30 08:07:00 +0100

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import { Eddi } from './models/Eddi';
+import { Harvi } from './models/Harvi';
+import { EddiBoost, EddiMode, ZappiBoostMode, ZappiChargeMode } from './models/Types';
+import { Zappi } from './models/Zappi';
 import { MyEnergi } from './MyEnergi';
-export { MyEnergi };
+export {
+    MyEnergi,
+    Eddi,
+    Zappi,
+    Harvi,
+    EddiBoost,
+    EddiMode,
+    ZappiBoostMode,
+    ZappiChargeMode
+};
