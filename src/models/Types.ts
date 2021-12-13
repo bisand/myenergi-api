7b6f4ce495445a312d792cf0791b1dd503b43796
André Biseth <andre@biseth.net>
2021-12-13 07:36:20 +0100

    Added heater status

--- src/models/Types.ts
+++ src/models/Types.ts
@@ -6,23 +6,31 @@ export enum EddiBoost {
     ManualHeater1 = '10-1',
     ManualHeater2 = '10-1',
     ManualRelay1 = '10-11',
-    ManualRelay2 = '10-12'
+    ManualRelay2 = '10-12',
+}
+
+export enum EddiHeaterStatus {
+    Paused = 1,
+    Diverting = 3,
+    Boost = 4,
+    MaxTempReached = 5,
+    Stopped = 6,
 }
 
 export enum EddiMode {
     Off = 0,
-    On = 1
+    On = 1,
 }
 
 export enum ZappiBoostMode {
     Manual = 10,
     Smart = 11,
-    Stop = 2
+    Stop = 2,
 }
 
 export enum ZappiChargeMode {
     Off = 4,
     Fast = 1,
     Eco = 2,
-    EcoPlus = 3
+    EcoPlus = 3,
 }
