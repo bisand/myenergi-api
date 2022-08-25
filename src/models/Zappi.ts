d831aec69a300e144a6ea1eb1899d6f5333ff89f
André Biseth <andre@biseth.net>
2022-08-25 07:28:51 +0200

    Added comments

--- src/models/Zappi.ts
+++ src/models/Zappi.ts
@@ -62,90 +62,105 @@ export interface Zappi {
      * @type {string}
      */
     sno: string;
+
     /**
      * Current Date
      * 
      * @type {string}
      */
     dat: string;
+
     /**
      * Current Time
      * 
      * @type {string}
      */
     tim: string;
+
     /**
      * Physical CT connection 1 value Watts
      * 
      * @type {number}
      */
     ectp1: number;
+
     /**
      * Physical CT connection 2 value Watts
      * 
      * @type {number}
      */
     ectp2: number;
+
     /**
      * Physical CT connection 3 value Watts
      * 
      * @type {number}
      */
     ectp3: number;
+
     /**
      * Physical CT connection 4 value Watts
      * 
      * @type {number}
      */
     ectp4: number;
+
     /**
      * Physical CT connection 5 value Watts
      * 
      * @type {number}
      */
     ectp5: number;
+
     /**
      * Physical CT connection 6 value Watts
      * 
      * @type {number}
      */
     ectp6: number;
+
     /**
      * CT 1 Name
      * 
      * @type {string}
      */
     ectt1: string;
+
     /**
      * CT 2 Name
      * 
      * @type {string}
      */
     ectt2: string;
+
     /**
      * CT 3 Name
      * 
      * @type {string}
      */
     ectt3: string;
+
     /**
      * CT 4 Name
      * 
      * @type {string}
      */
     ectt4: string;
+
     /**
      * CT 5 Name
      * 
      * @type {string}
      */
     ectt5: string;
+
     /**
      * CT 6 Name
      * 
      * @type {string}
      */
     ectt6: string;
+
     /**
      * Command Timer - 
      * 
@@ -159,91 +174,106 @@ export interface Zappi {
      * @type {number}
      */
     cmt: number;
+
     /**
      * Use Daylight Savings Time
      * 
      * @type {number}
      */
     dst: number;
+
     /**
      * Diversion amount Watts (does not appear if zero)
      * 
      * @type {number}
      */
     div: number;
+
     /**
      * Supply Frequency
      * 
      * @type {number}
      */
     frq: number;
+
     /**
      * Firmware Version
      * 
      * @type {number}
      */
     fwv: string;
+
     /**
      * Generated Watts
      * 
      * @type {number}
      */
     gen: number;
+
     /**
      * Watts from grid?
      * 
      * @type {number}
      */
     grd: number;
+
     /**
      * Minimum Green Level
      * 
      * @type {number}
      */
     mgl: number;
+
     /**
      * Number of Phases in the installation
      * 
      * @type {number}
      */
     pha: number;
+
     /**
      * priority
      * 
      * @type {number}
      */
     pri: number;
+
     /**
      * Status
      * 
-     *      0=Starting
-     *      1=Waiting for export or Paused
-     *      2=DSR
-     *      3=Diverting/Charging
-     *      4=Boosting
-     *      5=Complete 
+     *      0 = Starting
+     *      1 = Waiting for export
+     *      2 = DSR
+     *      3 = Diverting
+     *      4 = Boosting
+     *      5 = Hot
+     *      6 = Stopped
      * 
      * @type {number}
      */
     sta: number;
+
     /**
      * Timezone?
      * 
      * @type {number}
      */
     tz: number;
+
     /**
      * Voltage
      * 
      * @type {number}
      */
     vol: number;
+
     /**
      * Lates charge session. Charge added in KWh
      * 
      * @type {number}
      */
     che: number;
+
     /**
      * Lock Status
      * 
@@ -257,8 +287,9 @@ export interface Zappi {
      * @type {number}
      */
     lck: number;
+
     /**
-     * Charger Status
+     * Pilot State
      * 
      *      A = EV Disconnected,
      *      B1 = EV Connected,
@@ -270,48 +301,65 @@ export interface Zappi {
      * @type {string}
      */
     pst: string;
+
     /**
-     * Boost Mode. 
+     * Boost Manual. 
      *
      *      1 = ON
      *      0 = OFF
      *
-     * @example
-     * // Logic for selecting MANUAL or SMART
-     * if (tbh !== undefined && bsm === 1) 
-     *   MANUAL
-     * else
-     *   SMART
-     * 
      * @type {number}
      */
     bsm: number;
-    bst: number;
-    bss: number;
+
     /**
+     * Boost Smart. 
+     *
+     *      1 = ON
+     *      0 = OFF
+     *
+     * @type {number}
+     */
+     bss: number;
+
+    /**
+     * Boost Timed. 
+     *
+     *      1 = ON
+     *      0 = OFF
+     *
+     * @type {number}
+     */
+     bst: number;
+
+     /**
      * Smart Boost Start Time Hour
      * 
      * @type {number}
      */
     sbh: number;
+
     /**
      * Smart Boost KWh to add
      * 
      * @type {number}
      */
     sbk: number;
+
     /**
      * Smart Boost Start Time Minute
      * 
      * @type {number}
      */
     sbm: number;
+
     /**
      * boost hour?
      * 
      * @type {number}
      */
     tbh: number;
+
     /**
      * boost KWh
      * 
@@ -320,11 +368,13 @@ export interface Zappi {
      * @type {number}
      */
     tbk: number;
+
     /**
      * boost minute?
      * @type {number}
      */
     tbm: number;
+
     /**
      * Zappi Charge Mode
      * 
@@ -336,9 +386,11 @@ export interface Zappi {
      * @type {number}
      */
     zmo: number;
+
+    /** UNKNOWN */
     rdc: number;
     rac: number;
     rrac: number;
     zs: number;
-    zsl: number;    
+    zsl: number;
 }
